ff7ff233a6a229e109ce845b827b33053eee09b0
Stas Altsev <alstas@gmail.com>
2017-12-26 14:28:02 -0800

    * implement `createBoard` and `isWinner` methods

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -12,5 +12,79 @@ describe('/utils', () => {
       expect(utils).toHaveProperty('ask');
     });
   });
+
+  describe('#.createBoard', () => {
+    test('it exists', () => {
+      expect(utils).toHaveProperty('createBoard');
+    });
+
+    describe('.get', () => {
+      test('property', () => {
+        expect(utils.createBoard()).toHaveProperty('get');
+      });
+
+      test('returns actual value', () => {
+        expect(utils.createBoard().get(0)).toEqual('-');
+      });
+    });
+
+    describe('.setTurn', () => {
+      test('has such method', () => {
+        expect(utils.createBoard()).toHaveProperty('setTurn');
+      });
+
+      test('setTurn', () => {
+        let board = utils.createBoard();
+        let marker = 'm';
+
+        board.setTurn(0, marker);
+
+        expect(board.get(0)).toBe(marker);
+      });
+    });
+
+    describe('.print', () => {
+      test('has such method', () => {
+        expect(utils.createBoard()).toHaveProperty('print');
+      })
+    });
+  });
+
+  describe('#.isWinner', () => {
+    test('it exists', () => {
+      expect(utils).toHaveProperty('isWinner');
+    });
+
+    test('returns false when is not winner', () => {
+      const board = utils.createBoard();
+      expect(utils.isWinner(board, 'm')).toEqual(false);
+    });
+
+    const Identity = (val) => ({
+      map: fn => Identity(fn(val)),
+      toString: () => ``
+    });
+    const sets = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    sets.map((set) => {
+      test(`returns "true" for ${set} markers`, () => {
+        const marker = 'm';
+        const board = utils.createBoard();
+
+        set.map(section => board.setTurn(section, marker));
+
+        expect(utils.isWinner(board, marker)).toEqual(true);
+      });
+    });
+  });
 });
 
--- utils.js
+++ utils.js
@@ -1,8 +1,67 @@
 import readline from 'readline';
 
 export default {
+  createBoard,
   ask,
   runGeneratorWithPromises,
+  isWinner,
+}
+
+function isWinner(board, marker) {
+  return firstRow() || secondRow() || thirdRow()
+    || firstCol() || secondCol() || thirdCol()
+    || diagonalLeft() || diagonalRight();
+
+  function firstRow() {
+    return [0, 1, 2].every(hasMarker);
+  }
+
+  function secondRow() {
+    return [3, 4, 5].every(hasMarker);
+  }
+
+  function thirdRow() {
+    return [6, 7, 8].every(hasMarker);
+  }
+
+  function diagonalLeft() {
+    return [0, 4, 8].every(hasMarker);
+  }
+
+  function diagonalRight() {
+    return [2, 4, 6].every(hasMarker);
+  }
+
+  function firstCol() {
+    return [0, 3, 6].every(hasMarker);
+  }
+
+  function secondCol() {
+    return [1, 4, 7].every(hasMarker);
+  }
+
+  function thirdCol() {
+    return [2, 5, 8].every(hasMarker);
+  }
+
+  function hasMarker(section) {
+    return board.get(section) === marker;
+  }
+}
+
+const BoardIdentity = (board, emptySlotChar = '-') => ({
+  emptySlotChar,
+  get: (v = 0) => board[v],
+  toString: () => printBoard(board),
+  setTurn: (turn, marker) => {
+    board[turn] = marker;
+  },
+  print: () => printBoard(board),
+});
+
+export function createBoard(emptyCharacter = '-') {
+  let board = Array(9).fill(emptyCharacter);
+  return BoardIdentity(board, emptyCharacter);
 }
 
 export function ask(question) {
@@ -30,4 +89,17 @@ export function runGeneratorWithPromises(generator) {
       : next.value.then(v => run(iterator.next(v)))
         .catch(e => iterator.throw(e));
   }
+}
+
+function printBoard(board) {
+  process.stdout.write("\n");
+
+  for (let i = 0; i < 9; i++) {
+    process.stdout.write(`| ${board[i]} `);
+    if ((i + 1) % 3 === 0) {
+      process.stdout.write("|\n");
+    }
+  }
+
+  process.stdout.write("\n");
 }
